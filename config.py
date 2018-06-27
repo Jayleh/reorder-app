195e1a0a91470531043b6df0e1183885b47e4b9f
Jayleh <jaylehyun@gmail.com>
2018-06-27 11:21:08 -0700

    converting key into bytes

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-api_id = os.environ['api_id']
+api_id = (os.environ['api_id']).encode('utf-8')
 api_key = os.environ['api_key']
 mongodb_name = os.environ["MONGO_DBNAME"]
 mongo_uri = os.environ["MONGODB_URI"]
